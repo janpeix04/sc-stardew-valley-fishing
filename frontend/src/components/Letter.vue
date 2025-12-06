20ac60170a98dc2bbdc2009af3e065377d3d7fc5
SakanaDev <j.peig18@gmail.com>
2025-12-06 16:33:41 +0100

    Letter comoponent created

---
+++ frontend/src/components/Letter.vue
@@ -0,0 +1,32 @@
+<script>
+import BaseLetter from './BaseLetter.vue';
+import SmallFont from '@/assets/SmallFont.json';
+
+export default {
+    components: {
+        BaseLetter,
+    },
+    props: {
+        character: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            smallFont: SmallFont.Glyphs,
+        }
+    }
+}
+</script>
+
+<template>
+    <BaseLetter
+        :character="character" 
+        :x="smallFont[this.character].BoundsInTexture.X"
+        :y="smallFont[this.character].BoundsInTexture.Y"
+        :width="smallFont[this.character].BoundsInTexture.Width"
+        :height="smallFont[this.character].BoundsInTexture.Height"
+        :vertical-offset="smallFont[this.character].Cropping.Y"
+    />
+</template>
