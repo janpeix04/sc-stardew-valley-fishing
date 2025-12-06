9b6155cb861459082d3f41d8ec4e4e95c7a81f76
SakanaDev <j.peig18@gmail.com>
2025-12-06 10:22:46 +0100

    Base catch bar created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,15 @@ import BaseBackground from './components/BaseBackground.vue';
 import BaseActionButton from './components/BaseActionButton.vue';
 import BaseAttempt from './components/BaseAttempt.vue';
 import BaseCaptures from './components/BaseCaptures.vue';
+import BaseCatchBar from './components/BaseCatchBar.vue';
 
 export default {
   components: {
     BaseBackground,
     BaseActionButton,
     BaseAttempt,
-    BaseCaptures
+    BaseCaptures,
+    BaseCatchBar
   }
 }
 </script>
@@ -23,6 +25,8 @@ export default {
     <BaseAttempt :successful="true" :difficulty="'high'" />
     <BaseAttempt :successful="true" :difficulty="'legend'" />
   </BaseCaptures>
+
+  <BaseCatchBar />
 </template>
 
 <style>
