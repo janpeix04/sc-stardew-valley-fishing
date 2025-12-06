d07cfbe4cab454dabdc96a5dd75f056d418f0998
SakanaDev <j.peig18@gmail.com>
2025-12-06 10:07:53 +0100

    Base components from the creation of vue projecte have been deleted

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import BaseBackground from './components/BaseBackground.vue';
+import BaseActionButton from './components/BaseActionButton.vue';
+import BaseAttempt from './components/BaseAttempt.vue';
+import BaseCaptures from './components/BaseCaptures.vue';
+
+export default {
+  components: {
+    BaseBackground,
+    BaseActionButton,
+    BaseAttempt,
+    BaseCaptures
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <BaseBackground />
+  <BaseActionButton :text="'start'" :disabled="false" />
+  <BaseCaptures>
+    <BaseAttempt :successful="true" :difficulty="'low'" />
+    <BaseAttempt :successful="true" :difficulty="'medium'" />
+    <BaseAttempt :successful="true" :difficulty="'high'" />
+    <BaseAttempt :successful="true" :difficulty="'legend'" />
+  </BaseCaptures>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+img {
+  -webkit-user-drag: none;
 }
 </style>
