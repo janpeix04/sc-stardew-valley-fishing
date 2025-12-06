54c8cb9480bf1bb93022579fafbaf0c9cb0ca401
SakanaDev <j.peig18@gmail.com>
2025-12-06 17:34:19 +0100

    Catch bar function created

---
+++ backend/catch_bar.js
@@ -0,0 +1,41 @@
+import { 
+    CATCH_BAR_INITIAL_POSITION,
+    computeCatchBarCurrentPosition
+} from '../frontend/public/globals.js'
+
+export default function CatchBar(swappedDirectionCallback) {
+    let lastSwapAt;
+    let lastSwapPosition:;
+    let direction;
+
+    const start = () => {
+        lastSwapAt = new Date();
+        lastSwapPosition = CATCH_BAR_INITIAL_POSITION;
+        direction = "down";
+        swappedDirectionCallback(direction, lastSwapAt, lastSwapPosition);
+    };
+
+    const updateDirection = (newDirection) => {
+        if (newDirection === direction) return;
+
+        lastSwapPosition = computeCatchBarCurrentPosition(direction, lastSwapAt, lastSwapPosition);
+        lastSwapAt = new Date();
+        direction = newDirection;
+        swappedDirectionCallback(direction, lastSwapAt, lastSwapPosition);
+    };
+
+    const getInfo = () => {
+        return {
+            direction,
+            lastSwapAt,
+            lastSwapPosition,
+        }
+    };
+
+    return {
+        start,
+        updateDirection,
+        getInfo,
+    }
+
+}
