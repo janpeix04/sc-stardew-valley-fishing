4d54ed72e8c8eb662dabb4b3a16ee3f28cae6b9a
SakanaDev <j.peig18@gmail.com>
2025-12-06 11:32:51 +0100

    Base yellow fish bit indicator created

---
+++ frontend/src/components/BaseYellowIndicator.vue
@@ -0,0 +1,50 @@
+<script>
+import SmallExclamation from '@/assets/img/small_exclamation.png';
+import { SCALE_FACTOR } from '../../public/globals';
+
+export default {
+    props: {
+        showTrigger: {
+            type: Number,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            SCALE_FACTOR,
+            imgSrc: SmallExclamation,
+            inlineStyle: 'opacity: 0;'
+        }
+    },
+    methods: {
+        recursivelyCheckOpacity(ticks = 0) {
+            const computedStyle = window.getComputedStyle(this.$refs.fishBitIndicator)
+            if ('1' !== computedStyle.opacity) {
+                this.$nextTick(_ => this.recursivelyCheckOpacity(ticks + 1));
+            } else {
+                this.inlineStyle = `transform: translateY(-20px) scale(${SCALE_FACTOR}); opacity: 0; transition: transform 1s, opacity 1s;`
+            }
+        }
+    },
+    watch: {
+        showTrigger() {
+            this.inlineStyle = `transform: scale(${SCALE_FACTOR}); opacity: 1;`;
+            this.recursivelyCheckOpacity();
+        }
+    }
+}
+</script>
+
+<template>
+    <img id="fishBitIndicator" :src="imgSrc" alt="Fish bit indicator" ref="fishBitIndicator" :style="inlineStyle">
+</template>
+
+<style scoped>
+#fishBitIndicator {
+    position: absolute;
+    top: 430px;
+    left: 870px;
+    image-rendering: pixelated;
+    transform: scale(v-bind(SCALE_FACTOR));
+}
+</style>
