9fd1a0c5daa3329aedca9ad15d7e1f41a25de42d
SakanaDev <j.peig18@gmail.com>
2025-12-06 16:36:24 +0100

    Sentence component created

---
+++ frontend/src/components/Sentence.vue
@@ -0,0 +1,23 @@
+<script>
+import BaseSentence from './BaseSentence.vue';
+import Letter from './Letter.vue';
+
+export default {
+    components: {
+        BaseSentence,
+        Letter,
+    },
+    props: {
+        text: {
+            type: String,
+            required: true,
+        }
+    }
+}
+</script>
+
+<template>
+    <BaseSentence>
+        <Letter v-for="character in text" :character="character" />
+    </BaseSentence>
+</template>
