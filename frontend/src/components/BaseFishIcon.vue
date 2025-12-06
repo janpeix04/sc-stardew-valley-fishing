b7fd17a33ba4cee82962aa72d3de21ffe56c5ad1
SakanaDev <j.peig18@gmail.com>
2025-12-06 10:05:53 +0100

    Base fish icon created

---
+++ frontend/src/components/BaseFishIcon.vue
@@ -0,0 +1,39 @@
+<script>
+import Crab from '@/assets/img/crab.png';
+import Pufferfish from '@/assets/img/pufferfish.png';
+import Tuna from '@/assets/img/tuna.png';
+import Legend from '@/assets/img/legend.png';
+
+const fishTypeToFishSrc = {
+    "crab": Crab,
+    "pufferfish": Pufferfish,
+    "tuna": Tuna,
+    "legend": Legend
+}
+
+export default {
+    props: {
+        fishType: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            crab: Crab,
+            pufferfish: Pufferfish,
+            tuna: Tuna,
+            legend: Legend
+        };
+    },
+    computed: {
+        imgSrc() {
+            return fishTypeToFishSrc[this.fishType];
+        }
+    }
+}
+</script>
+
+<template>
+    <img :src="imgSrc" :alt="fishType">
+</template>
